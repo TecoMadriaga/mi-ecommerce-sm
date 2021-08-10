fcdbf7bdb52aa780eff60d5cb371a88cf1ad9437
Sergio Madriaga Ureta <31195363+TecoMadriaga@users.noreply.github.com>
2021-08-10 18:58:25 -0400

    Cambios para entrega 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+export default class NavBar extends PureComponent {
+  render() {
+    return (
+      <div className="header">
+        <div className="logo"></div>
+        <div className="menu">
+          <a href="#page1" className="menu-item active">Home</a>
+          <a href="#page2" className="menu-item">Nosotros</a>
+          <a href="#page3" className="menu-item">Catálogo</a>
+          <a href="#page4" className="menu-item">Contacto</a>
+        </div>
+      </div>
+    )
+  }
+}
